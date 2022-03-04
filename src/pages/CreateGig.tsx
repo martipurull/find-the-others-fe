5bbe0f989f29099c40e58937b8ba63bed1c97d48
martipurull <91315241+martipurull@users.noreply.github.com>
2022-03-04 15:11:19 +0100

    adds gigs search page

---
+++ src/pages/CreateGig.tsx
@@ -0,0 +1,18 @@
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import Input from '@mui/material/Input'
+import Select from '@mui/material/Select'
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
+
+export default function CreateGig() {
+    return (
+        <div>
+
+        </div>
+    )
+}
