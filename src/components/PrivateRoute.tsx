276fa896dad2237baedb3347b7e31e31bf643f9a
martipurull <91315241+martipurull@users.noreply.github.com>
2022-03-23 20:00:01 +0100

    adds private route functionality

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,11 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
+import useAxios from '../hooks/useAxios'
+import { addUserInfoAction } from '../redux/actions/actions'
+import { IInitialState } from '../types'
 
 interface IProps {
     children: JSX.Element
 }
 
 export default function PrivateRoute({ children }: IProps) {
-    const isLoggedIn = true
+    const dispatch = useDispatch()
+    const isLoggedIn = useSelector((state: IInitialState) => state.user.isLoggedIn)
+    const { axiosRequest } = useAxios()
+
+    const fetchUserInfo = async () => {
+        const response = await axiosRequest('/user/me', 'GET')
+        dispatch(addUserInfoAction(response.data))
+    }
+
+    if (isLoggedIn) fetchUserInfo()
 
     return isLoggedIn ? children : <Navigate to='/login' />
 }
